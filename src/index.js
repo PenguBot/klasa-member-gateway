b3bb58e6897dacc0af25f082992491c95a762727
Aditya N. Tripathi <adityatripathidelhi@gmail.com>
2020-09-24 11:24:34 +0530

    refactor: fixes for new klasa

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ module.exports = {
 	KlasaGuild: require('./lib/extensions/KlasaGuild'),
 	KlasaGuildMemberManager: require('./lib/extensions/KlasaGuildMemberManager'),
 	KlasaMember: require('./lib/extensions/KlasaMember'),
-	MemberGateway: require('./lib/settings/MemberGateway'),
 	Client: require('./lib/Client'),
 	[plugin]: require('./lib/Client')[plugin]
 };
